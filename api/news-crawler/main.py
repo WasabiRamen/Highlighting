02492d59f2a47ea585c855d770802f7ad74b0881
JoonheeKim (김준희) <58096790+WasabiRamen@users.noreply.github.com>
2025-12-25 11:11:41 +0000

    [news-crawler] 업데이트

--- api/news-crawler/main.py
+++ api/news-crawler/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 
+from tools.naver_news import NaverNews
+from tools.settings import settings
+
+naver_news = NaverNews(
+    client_id=settings.naver_client_id,
+    client_secret=settings.naver_client_secret
+)
+
+
 app = FastAPI(title="News Crawler Service")
 
 
@@ -11,3 +20,8 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
+
+@app.get("/last-news/{stock}")
+async def read_last_news(stock: str):
+    news = await naver_news.recent_5_news(stock)
+    return news
